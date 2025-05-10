1f2c0acc203bfbae4e9127a8a86657ecb02c113f
Oliver Lukesch <oliver.lukesch@gmail.com>
2025-05-10 15:28:06 +0200

    shadcn in place

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -49,6 +53,13 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+          <Button
+            variant="outline"
+            onClick={() => console.log("Button clicked!")}
+          >
+            Button
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
