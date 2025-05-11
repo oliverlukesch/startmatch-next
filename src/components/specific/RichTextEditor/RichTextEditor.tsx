b4922b0faf2297678d462cf705ffb7bcae1eccef
Oliver Lukesch <oliver.lukesch@gmail.com>
2025-05-11 12:29:31 +0200

    optimized blocknote a tiny bit more

--- src/components/specific/RichTextEditor/RichTextEditor.tsx
+++ src/components/specific/RichTextEditor/RichTextEditor.tsx
@@ -1,5 +1,11 @@
 'use client'
 
+import {
+  BlockNoteSchema,
+  defaultBlockSpecs,
+  defaultInlineContentSpecs,
+  defaultStyleSpecs,
+} from '@blocknote/core'
 import '@blocknote/core/fonts/inter.css'
 import {BlockNoteView} from '@blocknote/mantine'
 import '@blocknote/mantine/style.css'
@@ -7,7 +13,26 @@ import {useCreateBlockNote} from '@blocknote/react'
 
 import {cn} from '@/lib/utils'
 
+const schema = BlockNoteSchema.create({
+  blockSpecs: {
+    paragraph: defaultBlockSpecs.paragraph,
+    heading: defaultBlockSpecs.heading,
+    bulletListItem: defaultBlockSpecs.bulletListItem,
+    numberedListItem: defaultBlockSpecs.numberedListItem,
+  },
+  styleSpecs: {
+    bold: defaultStyleSpecs.bold,
+    italic: defaultStyleSpecs.italic,
+    underline: defaultStyleSpecs.underline,
+    strike: defaultStyleSpecs.strike,
+  },
+  inlineContentSpecs: {
+    text: defaultInlineContentSpecs.text,
+    link: defaultInlineContentSpecs.link,
+  },
+})
+
 export default function RichTextEditor({className}: {className?: string}) {
-  const editor = useCreateBlockNote()
+  const editor = useCreateBlockNote({animations: false, schema})
   return <BlockNoteView editor={editor} className={cn('rounded-md border p-1', className)} />
 }
