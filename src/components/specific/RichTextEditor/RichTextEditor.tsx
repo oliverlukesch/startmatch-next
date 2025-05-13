fe74ead8f4bc98e8d63dbb4d7f274964a5bf7782
Oliver Lukesch <oliver.lukesch@gmail.com>
2025-05-13 12:40:58 +0200

    basic collaboeration in place

--- src/components/specific/RichTextEditor/RichTextEditor.tsx
+++ src/components/specific/RichTextEditor/RichTextEditor.tsx
@@ -10,8 +10,21 @@ import '@blocknote/core/fonts/inter.css'
 import {BlockNoteView} from '@blocknote/mantine'
 import '@blocknote/mantine/style.css'
 import {useCreateBlockNote} from '@blocknote/react'
+import {TiptapCollabProvider} from '@hocuspocus/provider'
+import * as Y from 'yjs'
 
 import './style.css'
+
+const doc = new Y.Doc()
+
+const provider = new TiptapCollabProvider({
+  name: 'dev.document',
+  appId: 'y9w5pjo9',
+  // generated inside the TipTap dashboard, valid for 24 hours, needs to be refreshed
+  token:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDcxMzIyNTEsIm5iZiI6MTc0NzEzMjI1MSwiZXhwIjoxNzQ3MjE4NjUxLCJpc3MiOiJodHRwczovL2Nsb3VkLnRpcHRhcC5kZXYiLCJhdWQiOiJ5OXc1cGpvOSJ9.wyU7g4kxUyvz3yWXSgKgo4ivofH6w_xcurL6-1XtPyg',
+  document: doc,
+})
 
 const schema = BlockNoteSchema.create({
   blockSpecs: {
@@ -34,10 +47,19 @@ const schema = BlockNoteSchema.create({
 })
 
 export default function RichTextEditor(props: {className?: string}) {
-  const editor = useCreateBlockNote({animations: false, schema})
-
-  // this works, save for later reference
-  // console.log('editor', editor._tiptapEditor)
+  const editor = useCreateBlockNote({
+    animations: false,
+    schema,
+    collaboration: {
+      provider,
+      fragment: doc.getXmlFragment('document-store'),
+      user: {
+        name: `User: ${Math.round(Math.random() * 1000)}`,
+        color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+      },
+      showCursorLabels: 'always',
+    },
+  })
 
   return <BlockNoteView editor={editor} sideMenu={false} data-theming-primary-rte {...props} />
 }
